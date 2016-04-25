6c48938e8d878a1adb643c161bf39ad0da4f4967
Jim Bugwadia <jim@nirmata.com>
2016-04-25 11:30:07 -0700

    - major cleanup - now environment variables are used as defaults, and the command line overrides

--- envflag.go
+++ envflag.go
@@ -1,245 +1,175 @@
 package envflag
 
 import (
-	goflag "flag"
+	"flag"
 	"os"
 	"strconv"
+	"time"
 
-	flag "github.com/spf13/pflag"
+	pflag "github.com/spf13/pflag"
 )
 
-const (
-	// BitSize - conversions are 64-bit
-	BitSize = 64
-
-	// BaseNum - conversions are base-10
-	BaseNum = 10
-)
-
+// MergeFlags will merge flag.FlagSet to the pflag.FlagSet
 func MergeFlags() {
-	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)	
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 }
 
 // Parse parses the FlagSet
 func Parse() {
-	/*     Adding flags to goflag as well, glog requires flag.Parse before
-	 *      logging starts. If we don't add any flag in goflag, then flag.Parse
-	 *      fails saying flag is not defined.
-	 *      So, any flag we define we add to pflags and goflags both. Usage only
-	 *      prints pflags
+	/**
+	* Adding flags to goflag as well, glog requires flag.Parse before
+	* logging starts. If we don't add any flag in flag, then flag.Parse
+	* fails saying flag is not defined.
+	*
+	* So, for any flag we define we add to pflags and goflags both.
+	* Usage only prints pflags
 	 */
 
 	flag.Parse()
-	goflag.Parse()
+	pflag.Parse()
 }
 
-func Lookup(name string) *flag.Flag{
-	return flag.Lookup(name)
+// Lookup returns a flag value, or nil if it does not exist
+func Lookup(name string) *pflag.Flag {
+	return pflag.Lookup(name)
 }
+
 // Parsed returns true if FlagSet already parsed, false otherwise
 func Parsed() bool {
-
-	return flag.Parsed()
+	return pflag.Parsed()
 }
 
 // PrintDefaults prints default value for all elements
 func PrintDefaults() {
-
-	flag.PrintDefaults()
+	pflag.PrintDefaults()
 }
 
 // Set sets the name/value pair in the flag set
 func Set(name, value string) error {
-
-	flag.Set(name, value)
+	pflag.Set(name, value)
 	return nil
 }
 
+// Usage prints the flag usage
 func Usage() {
-
-	flag.Usage()
+	pflag.Usage()
 }
 
 func verifyNames(flagName, envName string) {
 	if flagName == "" && envName == "" {
-		panic("No flag name or env name given")
+		panic("No flag or environment name given for " + envName)
 	}
 }
 
-func lookupFlag(name string) bool {
-
-	if len(name) > 0 {
-		if flag.Lookup(name) != nil {
-			return true
-		}
+func lookupEnv(name string) string {
+	if name == "" {
+		return ""
 	}
 
-	return false
+	return os.Getenv(name)
 }
 
-func lookupEnv(name string) bool {
-
-	if len(name) > 0 {
-		envVal := os.Getenv(name)
-		if len(envVal) > 0 {
-			return true
-		}
-	}
-
-	return false
-}
-
-// Bool creates a new entry in the flag set with boolean value.
-// The environment value is overridden with the flag value if it exists
+// Bool creates a new entry in the flag set of boolean value.
+// The environment value used as a default, if it exists
 func Bool(flagName, envName string, value bool, usage string) *bool {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseBool(envValStr)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Bool(flagName, envVal, usage)
-		}
-		return &envVal
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = strconv.ParseBool(envValStr)
 	}
-	goflag.Bool(flagName, value, usage)
-	return flag.Bool(flagName, value, usage)
+
+	flag.Bool(flagName, value, usage)
+	return pflag.Bool(flagName, value, usage)
 }
 
 // Float64 creates a new entry in the flag set with float64 value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func Float64(flagName, envName string, value float64, usage string) *float64 {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseFloat(envValStr, BitSize)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Float64(flagName, envVal, usage)
-		}
-		return &envVal
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = strconv.ParseFloat(envValStr, 64)
 	}
-	goflag.Float64(flagName, value, usage)
-	return flag.Float64(flagName, value, usage)
+
+	flag.Float64(flagName, value, usage)
+	return pflag.Float64(flagName, value, usage)
 }
 
 // Int creates a new entry in the flag set with Int value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func Int(flagName, envName string, value int, usage string) *int {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseInt(envValStr, BaseNum, BitSize)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Int(flagName, int(envVal), usage)
-		}
-		envValInt := int(envVal)
-		return &envValInt
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = strconv.Atoi(envValStr)
 	}
-	goflag.Int(flagName, value, usage)
-	return flag.Int(flagName, value, usage)
+
+	flag.Int(flagName, value, usage)
+	return pflag.Int(flagName, value, usage)
 }
 
 // Int64 creates a new entry in the flag set with Int64 value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func Int64(flagName, envName string, value int64, usage string) *int64 {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseInt(envValStr, BaseNum, BitSize)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Int64(flagName, envVal, usage)
-		}
-		return &envVal
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = strconv.ParseInt(envValStr, 10, 64)
 	}
-	goflag.Int64(flagName, value, usage)
-	return flag.Int64(flagName, value, usage)
+
+	flag.Int64(flagName, value, usage)
+	return pflag.Int64(flagName, value, usage)
 }
 
 // String creates a new entry in the flag set with String value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func String(flagName, envName, value, usage string) *string {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envVal := os.Getenv(envName)
-		if flagValExists {
-			flag.Set(flagName, envVal)
-		} else {
-			flag.String(flagName, envVal, usage)
-		}
-		return &envVal
+	envVal := lookupEnv(envName)
+	if envVal != "" {
+		value = envVal
 	}
-	goflag.String(flagName, value, usage)
-	return flag.String(flagName, value, usage)
+
+	flag.String(flagName, value, usage)
+	return pflag.String(flagName, value, usage)
 }
 
 // UInt creates a new entry in the flag set with UInt value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func UInt(flagName, envName string, value uint, usage string) *uint {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseUint(envValStr, BaseNum, BitSize)
-		envValUint := uint(envVal)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Uint(flagName, envValUint, usage)
-		}
-		return &envValUint
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		ui64, _ := strconv.ParseUint(envValStr, 10, 64)
+		value = uint(ui64)
 	}
-	goflag.Uint(flagName, value, usage)
-	return flag.Uint(flagName, value, usage)
+
+	flag.Uint(flagName, value, usage)
+	return pflag.Uint(flagName, value, usage)
 }
 
 // UInt64 creates a new entry in the flag set with UInt64 value.
-// The environment value is overridden with the flag value if it exists
+// The environment value used as a default, if it exists
 func UInt64(flagName, envName string, value uint64, usage string) *uint64 {
-
 	verifyNames(flagName, envName)
-	flagValExists := lookupFlag(flagName)
-	envValExists := lookupEnv(envName)
-
-	if envValExists {
-		envValStr := os.Getenv(envName)
-		envVal, _ := strconv.ParseUint(envValStr, BaseNum, BitSize)
-		if flagValExists {
-			flag.Set(flagName, envValStr)
-		} else {
-			flag.Uint64(flagName, envVal, usage)
-		}
-		return &envVal
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = strconv.ParseUint(envValStr, 10, 64)
 	}
-	goflag.Uint64(flagName, value, usage)
-	return flag.Uint64(flagName, value, usage)
+
+	flag.Uint64(flagName, value, usage)
+	return pflag.Uint64(flagName, value, usage)
+}
+
+// Duration creates a new entry in the flag set with Duration value.
+// The environment value used as a default, if it exists
+func Duration(flagName, envName string, value time.Duration, usage string) *time.Duration {
+	verifyNames(flagName, envName)
+	envValStr := lookupEnv(envName)
+	if envValStr != "" {
+		value, _ = time.ParseDuration(envValStr)
+	}
+
+	flag.Duration(flagName, value, usage)
+	return pflag.Duration(flagName, value, usage)
 }
--- envflag_test.go
+++ envflag_test.go
@@ -3,295 +3,159 @@ package envflag
 import (
 	"os"
 	"testing"
-
-	"bitbucket.org/nirmata/go-envflag"
+	"time"
 )
 
-const (
-	boolValDefaultFlag   = true
-	f64ValDefaultFlag    = 3.14
-	intValDefaultFlag    = 3140
-	int64ValDefaultFlag  = 31400
-	uintValDefaultFlag   = 314000
-	uint64ValDefaultFlag = 3140000
+func TestFlagString(t *testing.T) {
+	flagS1 := String("S1", "S1", "hello1", "String")
+	Parse()
+	if *flagS1 != "hello1" {
+		t.Errorf("Error: flagS1 expected %v got %v", "hello1", *flagS1)
+		t.Fail()
+	}
+}
 
-	boolValDefaultFlagStr   = "true"
-	f64ValDefaultFlagStr    = "3.14"
-	intValDefaultFlagStr    = "3140"
-	int64ValDefaultFlagStr  = "31400"
-	uintValDefaultFlagStr   = "314000"
-	uint64ValDefaultFlagStr = "3140000"
+func TestEnvFlagString(t *testing.T) {
+	os.Setenv("S2", "hello2")
+	flagS2 := String("S2", "S2", "hello2", "String")
+	Parse()
+	if *flagS2 != "hello2" {
+		t.Errorf("Error: flagS2 expected %v got %v", "hello2", *flagS2)
+		t.Fail()
+	}
+}
 
-	boolValDefaultFlag2   = true
-	f64ValDefaultFlag2    = 4.13
-	intValDefaultFlag2    = 4130
-	int64ValDefaultFlag2  = 41300
-	uintValDefaultFlag2   = 413000
-	uint64ValDefaultFlag2 = 4130000
-
-	boolValDefaultFlag2Str   = "true"
-	f64ValDefaultFlag2Str    = "4.13"
-	intValDefaultFlag2Str    = "4130"
-	int64ValDefaultFlag2Str  = "41300"
-	uintValDefaultFlag2Str   = "413000"
-	uint64ValDefaultFlag2Str = "4130000"
-
-	boolValDefaultEnv   = false
-	f64ValDefaultEnv    = 8.13
-	intValDefaultEnv    = 8130
-	int64ValDefaultEnv  = 81300
-	uintValDefaultEnv   = 813000
-	uint64ValDefaultEnv = 8130000
-
-	boolValDefaultEnvStr   = "false"
-	f64ValDefaultEnvStr    = "8.13"
-	intValDefaultEnvStr    = "8130"
-	int64ValDefaultEnvStr  = "81300"
-	uintValDefaultEnvStr   = "813000"
-	uint64ValDefaultEnvStr = "8130000"
-)
-
-/* For testing purposes, we cannot declare these variables here since
-we're creating environment variables in the code.  We should call
-these functions after declaring the environment variables. Then
-call parse
-
-var (
-	flagB      = envflag.Bool("B", "B", boolValDefaultFlag, "Bool [BOOL]")
-	flagF64    = envflag.Float64("F64", "F64", f64ValDefaultFlag, "F64 [F64]")
-	flagInt    = envflag.Int("Int", "Int", intValDefaultFlag, "Int [INT]")
-	flagInt64  = envflag.Int64("Int64", "Int64", int64ValDefaultFlag, "Int64 [INT64]")
-	flagUint   = envflag.UInt("Uint", "Uint", uintValDefaultFlag, "Uint [UINT]")
-	flagUint64 = envflag.UInt64("Uint64", "Uint64", uint64ValDefaultFlag, "Uint64 [UINT64]")
-)
-*/
+func TestFlagBool(t *testing.T) {
+	flagB1 := Bool("B1", "B1", false, "Bool [BOOL]")
+	Parse()
+	if *flagB1 != false {
+		t.Errorf("Error: flagB1 expected %v got %v", false, *flagB1)
+		t.Fail()
+	}
+}
 
 func TestEnvFlagBool(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("B", boolValDefaultEnvStr); err != nil {
-		t.Log("B Setenv err:", err)
-		t.Fail()
-	}
-
-	flagB := envflag.Bool("B", "B", boolValDefaultFlag, "Bool [BOOL]")
-	envflag.Parse()
-
-	if *flagB != boolValDefaultEnv {
-
-		t.Errorf("Error: Unexpected values after processing")
+	os.Setenv("B2", "true")
+	flagB2 := Bool("B2", "B2", false, "Bool [BOOL]")
+	Parse()
+	if *flagB2 != true {
+		t.Errorf("Error: flagB2 expected %v got %v", true, *flagB2)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagF64(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("F64", f64ValDefaultEnvStr); err != nil {
-		t.Log("F64 Setenv err:", err)
-		t.Fail()
-	}
-
-	flagF64 := envflag.Float64("F64", "F64", f64ValDefaultFlag, "F64 [F64]")
-
-	if *flagF64 != f64ValDefaultEnv {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagInt(t *testing.T) {
+	flagI1 := Int("I1", "I1", 5, "Int")
+	Parse()
+	if *flagI1 != 5 {
+		t.Errorf("Error: flagI1 expected %v got %v", 5, *flagI1)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagInt(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("Int", intValDefaultEnvStr); err != nil {
-		t.Log("Int Setenv err:", err)
-		t.Fail()
-	}
-
-	flagInt := envflag.Int("Int", "Int", intValDefaultFlag, "Int [INT]")
-	envflag.Parse()
-
-	if *flagInt != int(intValDefaultEnv) {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagIntEnv(t *testing.T) {
+	os.Setenv("I2", "15")
+	flagI2 := Int("I2", "I2", 5, "Int")
+	Parse()
+	if *flagI2 != 15 {
+		t.Errorf("Error: flagI2 expected %v got %v", 15, *flagI2)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagInt64(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("Int64", int64ValDefaultEnvStr); err != nil {
-		t.Log("Int64 Setenv err:", err)
-		t.Fail()
-	}
-	flagInt64 := envflag.Int64("Int64", "Int64", int64ValDefaultFlag, "Int64 [INT64]")
-	envflag.Parse()
-
-	if *flagInt64 != int64ValDefaultEnv {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagInt64(t *testing.T) {
+	flagI3 := Int64("I3", "I3", 501, "Int")
+	Parse()
+	if *flagI3 != 501 {
+		t.Errorf("Error: flagI3 expected %v got %v", 501, *flagI3)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagUint(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("Uint", uintValDefaultEnvStr); err != nil {
-		t.Log("Uint Setenv err:", err)
-		t.Fail()
-	}
-
-	flagUint := envflag.UInt("Uint", "Uint", uintValDefaultFlag, "Uint [UINT]")
-	envflag.Parse()
-
-	if *flagUint != uint(uintValDefaultEnv) {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagIntEnv64(t *testing.T) {
+	os.Setenv("I4", "333")
+	flagI4 := Int("I4", "I4", 3, "Int")
+	Parse()
+	if *flagI4 != 333 {
+		t.Errorf("Error: flagI4 expected %v got %v", 333, *flagI4)
 		t.Fail()
 	}
 }
 
-// TestEnvFlag tests if we can get flags and environment variables,
-// and if the environment variables can override the flags
-func TestEnvFlagUint64(t *testing.T) {
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	if err := os.Setenv("Uint64", uint64ValDefaultEnvStr); err != nil {
-		t.Log("Uint64 Setenv err:", err)
-		t.Fail()
-	}
-
-	flagUint64 := envflag.UInt64("Uint64", "Uint64", uint64ValDefaultFlag, "Uint64 [UINT64]")
-	envflag.Parse()
-
-	if *flagUint64 != uint64ValDefaultEnv {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagUInt(t *testing.T) {
+	flagUI := UInt64("UI", "UI", 1235, "UInt")
+	Parse()
+	if *flagUI != 1235 {
+		t.Errorf("Error: flagUI expected %v got %v", 1235, *flagUI)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagBoolNoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagB2 := envflag.Bool("B2", "B2", boolValDefaultFlag2, "Bool2 [BOOL2]")
-	envflag.Parse()
-
-	if *flagB2 != boolValDefaultFlag2 {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagUIntEnv(t *testing.T) {
+	os.Setenv("UI2", "657575")
+	flagUI2 := Int("UI2", "UI2", 0, "UInt")
+	Parse()
+	if *flagUI2 != 657575 {
+		t.Errorf("Error: flagUI2 expected %v got %v", 657575, *flagUI2)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagF64NoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagF642 := envflag.Float64("F642", "F642", f64ValDefaultFlag2, "F642 [F642]")
-	envflag.Parse()
-
-	if *flagF642 != f64ValDefaultFlag2 {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagUInt64(t *testing.T) {
+	flagUI3 := UInt64("UI3", "UI3", 919, "UInt64")
+	Parse()
+	if *flagUI3 != 919 {
+		t.Errorf("Error: flagUI3 expected %v got %v", 919, *flagUI3)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagIntNoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagInt2 := envflag.Int("Int2", "Int2", intValDefaultFlag2, "Int2 [INT2]")
-	envflag.Parse()
-
-	if *flagInt2 != int(intValDefaultFlag2) {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagUIntEnv64(t *testing.T) {
+	os.Setenv("UI4", "3232")
+	flagUI4 := Int("UI4", "UI4", 0, "UInt64")
+	Parse()
+	if *flagUI4 != 3232 {
+		t.Errorf("Error: flagUI4 expected %v got %v", 3232, *flagUI4)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagInt64NoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagInt642 := envflag.Int64("Int642", "Int642", int64ValDefaultFlag2, "Int642 [INT642]")
-	envflag.Parse()
-
-	if *flagInt642 != int64ValDefaultFlag2 {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagFloat64(t *testing.T) {
+	flagF64 := Float64("F64", "F64", 65.12, "Float64")
+	Parse()
+	if *flagF64 != 65.12 {
+		t.Errorf("Error: flagF64 expected %v got %v", 65.12, *flagF64)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagIntUintNoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagUint2 := envflag.UInt("Uint2", "Uint2", uintValDefaultFlag2, "Uint2 [UINT2]")
-	envflag.Parse()
-
-	if *flagUint2 != uint(uintValDefaultFlag2) {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagFloatEnv64(t *testing.T) {
+	os.Setenv("F64-2", "981.282")
+	flagF642 := Float64("F64-2", "F64-2", 0.0, "Float64")
+	Parse()
+	if *flagF642 != 981.282 {
+		t.Errorf("Error: flag642 expected %v got %v", 981.282, *flagF642)
 		t.Fail()
 	}
 }
 
-func TestEnvFlagIntUint64NoOverride(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	flagUint642 := envflag.UInt64("Uint642", "Uint642", uint64ValDefaultFlag2, "Uint642 [UINT642]")
-	envflag.Parse()
-
-	if *flagUint642 != uint64ValDefaultFlag2 {
-
-		t.Errorf("Error: Unexpected values after processing")
+func TestFlagDuration(t *testing.T) {
+	d := time.Second * 20
+	flagD := Duration("D", "D", d, "Duration")
+	Parse()
+	if *flagD != d {
+		t.Errorf("Error: flagF64 expected %v got %v", d, *flagD)
 		t.Fail()
 	}
 }
 
-func TestPrintDefaults(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	envflag.UInt("Uint21", "Uint21", uintValDefaultFlag2, "Uint21 [UINT21]")
-
-	envflag.UInt64("Uint6421", "Uint6421", uint64ValDefaultFlag2, "Uint6421 [UINT6421]")
-	t.Logf(">>>>> Printing Defaults <<<<<")
-	envflag.PrintDefaults()
-}
-
-func TestPrintUsage(t *testing.T) {
-
-	envflag.Set("logtostderr", "true")
-	envflag.Set("v", "3")
-
-	t.Logf(">>>>> Printing Usage <<<<<")
-	envflag.Usage()
+func TestFlagFloatEnvDuration(t *testing.T) {
+	d := time.Minute * 5
+	os.Setenv("D2", d.String())
+	flagD2 := Duration("D2", "D2", 0, "Duration")
+	Parse()
+	if *flagD2 != d {
+		t.Errorf("Error: flag642 expected %v got %v", d, *flagD2)
+		t.Fail()
+	}
 }
